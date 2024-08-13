05bba98339a4fd71cb2c8383aad22192cccb9d8c
Artur Mygelski <arturmygelski@gmail.com>
2024-08-13 15:55:21 +0200

    done

--- file_handler.py
+++ file_handler.py
@@ -1,28 +1,20 @@
 import csv
-
-class FileHandler:
+import pickle
+from abc import ABC, abstractmethod
+class FileHandler(ABC):
     def __init__(self, input_file, output_file, changes):
         self.input_file = input_file
         self.output_file = output_file
         self.matrix = self.load_data_from_file()
         self.changes = changes
-        print(self.matrix)
-        print(self.changes)
 
+    @abstractmethod
     def load_data_from_file(self):
-        temp_matrix = []
-        with open(self.input_file) as file:
-            reader = csv.reader(file)
-            for line in reader:
-                temp_matrix.append(line)
-        return temp_matrix
+        pass
 
+    @abstractmethod
     def save_data_to_file(self):
-        with open(self.output_file, mode="w+", newline="") as file:
-            writer = csv.writer(file)
-            for line in self.matrix:
-                writer.writerow(line)
-
+        pass
 
     def transform(self):
         for transformation in self.changes:
@@ -35,3 +27,40 @@ class FileHandler:
 
         for row in self.matrix:
             print(",".join(row))
+
+class CSVFileHandler(FileHandler):
+    @abstractmethod
+    def load_data_from_file(self):
+        temp_matrix = []
+        with open(self.input_file) as file:
+            reader = csv.reader(file)
+            for line in reader:
+                temp_matrix.append(line)
+        return temp_matrix
+
+
+    @abstractmethod
+    def save_data_to_file(self):
+        with open(self.output_file, mode="w+", newline="") as file:
+            writer = csv.writer(file)
+            for line in self.matrix:
+                writer.writerow(line)
+
+class PickleHandler(FileHandler):
+    def load_data_from_file(self):
+        with open(self.input_file, mode="rb") as file:
+            return pickle.load(file)
+
+    def save_data_to_file(self):
+        with open(self.output_file, mode="wb") as file:
+            pickle.dump(self.matrix, file)
+
+class TXTFileHandler(FileHandler):
+    def load_data_from_file(self):
+        with open(self.input_file) as file:
+            return [line.split() for line in file]
+
+    def save_data_to_file(self):
+        with open(self.output_file, mode="w") as file:
+            for line in self.matrix:
+                file.write(" ".join(line) + "\n")
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import sys
-from file_handler import FileHandler
+from file_handler import CSVFileHandler, PickleHandler, TXTFileHandler
 
-arguments = sys.argv
-print(arguments)
-handler = FileHandler(input_file=arguments[1], output_file=arguments[2], changes=arguments[3:])
-handler.transform()
-handler.save_data_to_file()
+def get_handler_class(input_file):
+    if input_file.endswith(".csv"):
+        return CSVFileHandler
+    elif input_file.endswith(".pickle"):
+        return PickleHandler
+    elif input_file.endswith(".txt"):
+        return TXTFileHandler
+    else:
+        raise ValueError("nieobsługiwany format pliku")
+
+if __name__ == "__main__":
+    arguments = sys.argv
+    handler_class = get_handler_class(arguments[1])
+    handler = handler_class(input_file=arguments[1], output_file=arguments[2], changes=arguments[3:])
+    handler.transform()
+    handler.save_data_to_file()
